fe1a7d33cf62bc6be04eb54995490508b46fd8cd
buildconnect-backend <mavie907@gmail.com>
2026-02-20 01:29:18 +0300

    Update admin.js
    Add search route

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -8,6 +8,7 @@ const {
   getAllProjects,
   updateProjectStatus,
   deleteProject,
+  globalSearch,
 } = require('../controllers/admin');
 const { authenticate } = require('../middleware/auth');
 const { isAdmin } = require('../middleware/admin');
@@ -31,5 +32,21 @@ router.delete('/users/:id', deleteUser);
 router.get('/projects', getAllProjects);
 router.put('/projects/:id', updateProjectStatus);
 router.delete('/projects/:id', deleteProject);
+
+const globalSearch = async (req, res) => {
+  try {
+    const { q } = req.query;
+    
+    if (!q || q.length < 2) {
+      return res.json({ 
+        success: true, 
+        data: { users: [], projects: [] },
+        message: 'Query too short'
+      });
+    }
+
+    const { Op } = require('sequelize');
+    const User = require('../models/User');
+    const RFP = require('../models/RFP');
 
 module.exports = router;
