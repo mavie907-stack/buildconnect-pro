2695f15b68d93a0f5b89d6ec815b13224c8c67c3
buildconnect-backend <mavie907@gmail.com>
2026-02-26 02:18:17 +0300

    Add files via upload

--- src/models/Message.js
+++ src/models/Message.js
@@ -1,1 +1,41 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/database');
 
+class Message extends Model {}
+
+Message.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    sender_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    receiver_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    subject: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: '',
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    is_read: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'messages',
+  }
+);
+
+module.exports = Message;
