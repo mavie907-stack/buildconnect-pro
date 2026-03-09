a89dda82017596a6093fa4cca5ed36a6a36ed0e6
buildconnect-backend <mavie907@gmail.com>
2026-03-10 02:00:22 +0300

    Update Message.js

--- src/models/Message.js
+++ src/models/Message.js
@@ -1,37 +1,42 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../config/database');
 
-const Message = sequelize.define('Message', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+class Message extends Model {}
+
+Message.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    sender_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    receiver_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    subject: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: '',
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    is_read: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
   },
-  sender_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  receiver_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  subject: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  body: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  is_read: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  },
-}, {
-  tableName: 'messages',
-  timestamps: true,
-  createdAt: 'createdAt',
-  updatedAt: 'updatedAt',
-});
+  {
+    sequelize,
+    tableName: 'messages',
+  }
+);
 
 module.exports = Message;
+
