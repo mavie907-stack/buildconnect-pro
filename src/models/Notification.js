feff7013c7e88a7adc8bf11d058bff9c1a97a9f0
buildconnect-backend <mavie907@gmail.com>
2026-03-10 22:25:27 +0300

    Update Notification.js

--- src/models/Notification.js
+++ src/models/Notification.js
@@ -2,12 +2,12 @@ const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../config/database');
 class Notification extends Model {}
 Notification.init({
-  id:         { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
-  user_id:    { type: DataTypes.UUID, allowNull: false },
-  type:       { type: DataTypes.STRING, allowNull: false },
-  title:      { type: DataTypes.STRING, allowNull: true },
-  message:    { type: DataTypes.TEXT, allowNull: true },
-  is_read:    { type: DataTypes.BOOLEAN, defaultValue: false },
-  data:       { type: DataTypes.JSON, defaultValue: {} },
+  id:      { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+  user_id: { type: DataTypes.UUID, allowNull: false },
+  type:    { type: DataTypes.STRING, allowNull: false },
+  title:   { type: DataTypes.STRING, allowNull: true },
+  message: { type: DataTypes.TEXT,   allowNull: true },
+  is_read: { type: DataTypes.BOOLEAN, defaultValue: false },
+  data:    { type: DataTypes.JSON,   defaultValue: {} },
 },{ sequelize, tableName: 'notifications' });
 module.exports = Notification;
