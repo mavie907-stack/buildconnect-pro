e31533aefef098f64fdfedf50946d9773b203256
buildconnect-backend <mavie907@gmail.com>
2026-03-10 23:01:34 +0300

    Add files via upload

---
+++ src/routes/admin.js
@@ -0,0 +1,31 @@
+const { Router } = require('express');
+const {
+  getStats, getAllUsers, getUserDetails, updateUser, deleteUser,
+  getAllProjects, updateProjectStatus, deleteProject, globalSearch,
+} = require('../controllers/admin');
+const { authenticate } = require('../middleware/auth');
+const { isAdmin }      = require('../middleware/admin');
+
+const router = Router();
+
+router.use(authenticate);
+router.use(isAdmin);
+
+// Dashboard
+router.get('/stats', getStats);
+
+// Users
+router.get('/users',        getAllUsers);
+router.get('/users/:id',    getUserDetails);
+router.put('/users/:id',    updateUser);
+router.delete('/users/:id', deleteUser);
+
+// Projects
+router.get('/projects',        getAllProjects);
+router.put('/projects/:id',    updateProjectStatus);
+router.delete('/projects/:id', deleteProject);
+
+// Search
+router.get('/search', globalSearch);
+
+module.exports = router;
