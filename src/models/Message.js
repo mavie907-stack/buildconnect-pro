87cf331faf5a24f2ec2ff615a47803011d547bb8
buildconnect-backend <mavie907@gmail.com>
2026-03-10 22:26:16 +0300

    Update Message.js

--- src/models/Message.js
+++ src/models/Message.js
@@ -5,7 +5,8 @@ Message.init({
   id:          { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
   sender_id:   { type: DataTypes.UUID, allowNull: false },
   receiver_id: { type: DataTypes.UUID, allowNull: false },
-  content:     { type: DataTypes.TEXT, allowNull: false },
+  subject:     { type: DataTypes.STRING, allowNull: true },
+  body:        { type: DataTypes.TEXT, allowNull: false },
   is_read:     { type: DataTypes.BOOLEAN, defaultValue: false },
   rfp_id:      { type: DataTypes.UUID, allowNull: true },
 },{ sequelize, tableName: 'messages' });
