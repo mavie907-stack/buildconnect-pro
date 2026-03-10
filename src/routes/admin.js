890b9e00ce44e7146a03aa2b22c51d606232fac1
buildconnect-backend <mavie907@gmail.com>
2026-03-10 23:42:07 +0300

    Update admin.js

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,4 +1,3 @@
-const { Router } = require('express');
 const {
   getStats, getAllUsers, getUserDetails, updateUser, deleteUser,
   getAllProjects, updateProjectStatus, deleteProject, globalSearch,
