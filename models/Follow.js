39767df5e406e81b6cb52df38b78e91d2d5b1241
buildconnect-backend <mavie907@gmail.com>
2026-03-12 22:55:23 +0300

    Add files via upload

---
+++ models/Follow.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Follow = sequelize.define('Follow', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  follower_id:  { type: DataTypes.UUID, allowNull: false },
+  following_id: { type: DataTypes.UUID, allowNull: false },
+}, {
+  tableName: 'follows',
+  timestamps: true,
+  indexes: [{ unique: true, fields: ['follower_id', 'following_id'] }],
+});
+
+module.exports = Follow;
